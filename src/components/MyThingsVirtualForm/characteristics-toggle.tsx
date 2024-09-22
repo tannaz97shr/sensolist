d4989277a2767a435cc384dd7193d4919876376b
Tannaz Shirzadi <tia.shr97@gmail.com>
2024-09-22 12:10:09 +0300

    feat: virtual thing form step three

--- src/components/MyThingsVirtualForm/characteristics-toggle.tsx
+++ src/components/MyThingsVirtualForm/characteristics-toggle.tsx
@@ -11,6 +11,9 @@ export function CharacteristicToggle(props: CharacteristicOptionsProps) {
     <div className="flex justify-between dark:text-white">
       {props.sensor.character}
       <ToggleSwitch
+        className={`${
+          !!props.sensor?.active && "flowbite_toggle_switch_active"
+        }`}
         checked={!!props.sensor?.active}
         onChange={() =>
           props.setSensorData({ ...props.sensor, active: !props.sensor.active })
