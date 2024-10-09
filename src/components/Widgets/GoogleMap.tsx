875c19f719865e4543a172f57e3693b703e29cb8
Tannaz Shirzadi <tia.shr97@gmail.com>
2024-10-09 12:55:01 +0300

    feat: map widgets

--- src/components/Widgets/GoogleMap.tsx
+++ src/components/Widgets/GoogleMap.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { _getWidgetData } from "@/ApiCall/widgets";
+import { getWidgetData } from "@/ApiCall/widgets";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import { useEffect, useState } from "react";
@@ -14,7 +14,10 @@ interface GoogleMapProps {
 }
 
 export default function GoogleMap({ senderId, name }: GoogleMapProps) {
-  const [widgetData, setWidgetData] = useState<{ lat: number; lng: number }>();
+  const [widgetData, setWidgetData] = useState<{
+    lat: number;
+    lng: number;
+  } | null>();
   const [seconds, setSeconds] = useState<number>(10);
   const [loading, setLoading] = useState<boolean>(false);
   let loctionIcon = L.icon({ iconUrl: "/assets/location.png" });
@@ -23,16 +26,24 @@ export default function GoogleMap({ senderId, name }: GoogleMapProps) {
       const getData = async () => {
         if (senderId) {
           setLoading(true);
+          const response = await getWidgetData(
+            senderId,
+            ["Longitude", "Latitude"],
+            1,
+            1
+          );
           setLoading(false);
-          const response = await _getWidgetData(senderId, [
-            "Longitude",
-            "Latitude",
-          ]);
-          //response.Longitude?.data[0].payload
-          setWidgetData({
-            lng: response.Longitude?.data[0].payload,
-            lat: response.Latitude?.data[0].payload,
-          });
+          if (response.charactersData) {
+            console.log("open street responseeeee", response);
+            setWidgetData(
+              response.charactersData?.length < 3
+                ? {
+                    lng: Number(response.charactersData[0].data[0].payload),
+                    lat: Number(response.charactersData[1].data[0].payload),
+                  }
+                : null
+            );
+          }
         }
       };
       getData();
@@ -47,27 +58,36 @@ export default function GoogleMap({ senderId, name }: GoogleMapProps) {
   }, [senderId, seconds]);
 
   return (
-    <div className="w-full aspect-video ">
-      {loading ? (
-        <Spinner />
-      ) : (
-        widgetData && (
-          <MapContainer
-            className=" w-full aspect-video overflow-hidden z-0"
-            center={widgetData}
-            zoom={13}
-            scrollWheelZoom={false}
-          >
-            <TileLayer
-              attribution="Google Maps"
-              url="https://www.google.cn/maps/vt?lyrs=m@189&gl=cn&x={x}&y={y}&z={z}"
-            />
-
-            <Marker position={widgetData} icon={loctionIcon}></Marker>
-            <RecenterMap lat={widgetData.lat} lng={widgetData.lng} />
-          </MapContainer>
+    <div className=" bg-black-opacity-50 dark:bg-white-opacity-50 mt-10 p-6 min-h-[calc(100%-140px)] flex flex-col">
+      {!widgetData ? (
+        loading ? (
+          <div className="flex h-full flex-1">
+            <Spinner className="m-auto" />
+          </div>
+        ) : (
+          <div className="flex h-full flex-1">
+            <span className="m-auto">No Data available!</span>
+          </div>
         )
+      ) : (
+        <MapContainer
+          className=" w-full overflow-hidden z-0 flex-1"
+          center={widgetData}
+          zoom={13}
+          scrollWheelZoom={false}
+        >
+          <TileLayer
+            attribution="Google Maps"
+            url="https://www.google.cn/maps/vt?lyrs=m@189&gl=cn&x={x}&y={y}&z={z}"
+          />
+
+          <Marker position={widgetData} icon={loctionIcon}></Marker>
+          <RecenterMap lat={widgetData.lat} lng={widgetData.lng} />
+        </MapContainer>
       )}
+      <div className=" text-neutral-7 dark:text-neutral-6 mx-auto w-fit mt-6 text-xs">
+        Last Update {seconds} seconds ago
+      </div>
     </div>
   );
 }
