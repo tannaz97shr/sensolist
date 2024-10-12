cb4a65c6887fe246dfa7da6fb92f29401bb2f097
Tannaz Shirzadi <tia.shr97@gmail.com>
2024-10-12 21:30:42 +0300

    feat: air quality co2 size

--- src/components/Widgets/index.tsx
+++ src/components/Widgets/index.tsx
@@ -159,9 +159,12 @@ export default function DashboardWidgets({
   const calcWidgetLayout = (simple: boolean, name: string, i: number) => {
     if (simple) {
       return name === "simple CO2 chart card"
-        ? { x: 4 * i, y: 0, w: 2, h: 8 }
+        ? { x: 4 * i, y: 0, w: 2, h: 6 }
         : { x: 4 * i, y: 0, w: 4, h: 18 };
     } else {
+      return name === "simple CO2 chart card"
+        ? { x: 4 * i, y: 0, w: 3, h: 12 }
+        : { x: 4 * i, y: 0, w: 4, h: 18 };
     }
   };
   return (
